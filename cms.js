0a08a7cda5323171ef819e92b7115ccc81a0ea21
Eleftherios Moschos <tmoschos@bigpond.com>
2020-08-01 23:05:38 +0930

    Created seed.sql and made SQL connection in cms.js

--- cms.js
+++ cms.js
@@ -1,1 +1,25 @@
-// NEW FILE CREATION
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+
+    port: 3306,
+
+    user: "root",
+
+    password: "HiAll08",
+    database: "seed_db"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    connection.query("SELECT * FROM employee", function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.log(res);
+    });
+    connection.end()
+});
---
+++ db/seed.sql
@@ -0,0 +1,40 @@
+DROP DATABASE IF EXISTS seed_db;
+CREATE database seed_db;
+USE seed_db;
+
+CREATE TABLE department (  
+id  INT NOT NULL AUTO_INCREMENT,
+name VARCHAR(30) NOT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+id INT NOT NULL AUTO_INCREMENT,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL (10, 2)
+department_id INT NOT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+id INT NOT NULL AUTO_INCREMENT, 
+first_name VARCHAR(30), 
+last_name VARCHAR(30),
+role_id INT NOT NULL,
+manager_id INT 
+PRIMARY KEY (id)
+);
+
+INSERT INTO department (name)
+VALUES ("Maintenance");
+
+INSERT INTO department (name)
+VALUES ("Operations");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Mechanic", 75000.00, 1);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Terry", "Moschos", 1, 0);
+
+SELECT * FROM employee;
