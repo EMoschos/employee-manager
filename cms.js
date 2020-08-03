bc557b6a12d84c1de9588643195eb77b45664877
Eleftherios Moschos <tmoschos@bigpond.com>
2020-08-03 23:39:48 +0930

    Tested functions to get info from DB to show in inquirer

--- cms.js
+++ cms.js
@@ -23,60 +23,70 @@ connection.connect(function (err) {
     });
 });
 
+//Testing to see how to get info to show in inquirer
 function cmsStart() {
-    inquirer.prompt([
-        {
-            name: "nameFirst",
-            type: "input",
-            message: "What is the employees first name?"
-        },
-        {
-            name: "nameLast",
-            type: "input",
-            message: "What is the employees last name?"
-        },
-        {
-            name: "role",
-            type: "input",
-            message: "What is the employees role?"
-        },
-        {
-            name: "depart",
-            type: "input",
-            message: "What department will the employee work in?"
+    var select = connection.query(
+        "SELECT name FROM department",
+        function (err, res) {
+            if (err) throw err;
+            console.log(JSON.stringify(res));
+            console.log(res[0].name);
+            inquirer.prompt([
+                {
+                    name: "nameFirst",
+                    type: "input",
+                    message: "What is the employees first name?"
+                },
+                {
+                    name: "nameLast",
+                    type: "input",
+                    message: "What is the employees last name?"
+                },
+                {
+                    name: "role",
+                    type: "input",
+                    message: res[0].name//Should print maintenance
+                },
+                {
+                    name: "depart",
+                    type: "input",
+                    message: "What department will the employee work in?"
+                }
+            ]).then(function (response) {
+                connection.query(
+                    "INSERT INTO department SET ?",
+                    {
+                        name: response.depart,
+                    },
+                    function (err, res) {
+                        if (err) throw err;
+                        console.log("New department Added");
+                    }
+                );
+                connection.query(
+                    "INSERT INTO role SET ?",
+                    {
+                        title: response.role,
+                    },
+                    function (err, res) {
+                        if (err) throw err;
+                        console.log("New role Added" + res);
+                    }
+                );
+                connection.query(
+                    "INSERT INTO employee SET ?",
+                    {
+                        first_name: response.nameFirst,
+                        last_name: response.nameLast,
+                    },
+                    function (err, res) {
+                        if (err) throw err;
+                        console.log("New Empliyee Added" + res);
+                        connection.end();
+                    }
+                );
+            })
         }
-    ]).then(function (response) {
-        connection.query(
-            "INSERT INTO department SET ?",
-            {
-                name: response.depart,
-            },
-            function (err, res) {
-                if (err) throw err;
-                console.log("New department Added");
-            }
-        );
-        connection.query(
-            "INSERT INTO role SET ?",
-            {
-                title: response.role,
-            },
-            function (err, res) {
-                if (err) throw err;
-                console.log("New role Added" + res);
-            }
-        );
-        connection.query(
-            "INSERT INTO employee SET ?",
-            {
-                first_name: response.nameFirst,
-                last_name: response.nameLast,
-            },
-            function (err, res) {
-                if (err) throw err;
-                console.log("New Empliyee Added" + res);
-                connection.end();
-            }
-        );
-    })
+    );
+    
 }
